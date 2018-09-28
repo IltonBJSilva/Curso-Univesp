fc8d0a5d52071bd6a9cae525ef9abe746e315473
Ilton Batista <43584705+IltonBJSilva@users.noreply.github.com>
2018-09-28 15:52:44 -0300

    Areas da casa

---
+++ Aula5.java
@@ -0,0 +1,14 @@
+public class Aula5 {
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Programa para calcular \n area na casal");
+        System.out.println(10*10);
+        System.out.println("A Area da sala e " + 37 * 20);
+        System.out.println("A Area do quarto e " + 10 * 10);
+        System.out.println("A area do banheiro e " + 6* 7);
+        System.out.println("A Area total e "+ 37*20 +10*10 + 6*7);
+
+    }
+
+}
