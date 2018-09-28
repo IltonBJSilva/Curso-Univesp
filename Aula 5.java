23f2908df6222dfc683af328bd6951f20870a7c2
IltonBJSilva <xlsonplay@gmail.com>
2018-09-28 15:39:13 -0300

    Novo programa da aula 05

--- Aula 5.java
+++ Aula 5.java
@@ -3,6 +3,8 @@ public class PrimeiroPrograma {
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
         System.out.println("Programa para calcular \n area na casal");
+        System.out.println(10*10);
+
     }
 
 }
