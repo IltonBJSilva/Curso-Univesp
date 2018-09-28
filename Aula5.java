893ac43c9d9dfaf1fbad30e03ca2ec6b1a90e028
Ilton Batista <43584705+IltonBJSilva@users.noreply.github.com>
2018-09-28 16:41:59 -0300

    Programa em java testando muitos caso

--- Aula5.java
+++ Aula5.java
@@ -2,12 +2,36 @@ public class Aula5 {
 
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
+        //Variaveis
+        int areaq,areab;
+        int areas;
+        int areat;
+        
+        //Variaveis sendo usadas
+        areas= 37*20;
+        areab = 7*9;   
+        areaq= 10*10;
+        areat= areaq + areas+areab;
+        //Mas simples o de baixo
+        System.out.println("Programa para calcular \n area na casal");
+        System.out.println(10 * 10);
+        System.out.println("A Area da sala e " + areas);
+        System.out.println("A Area do quarto e " + areaq);
+        System.out.println("A area do banheiro e " +areas);
+        System.out.println("A Area total e " + areat);
+        System.out.println("FIM DO RAPIDO");
+        //Areas sendo calculadas
         System.out.println("Programa para calcular \n area na casal");
         System.out.println(10*10);
         System.out.println("A Area da sala e " + 37 * 20);
         System.out.println("A Area do quarto e " + 10 * 10);
-        System.out.println("A area do banheiro e " + 6* 7);
-        System.out.println("A Area total e "+ 37*20 +10*10 + 6*7);
+        System.out.println("A area do banheiro e " + 7* 9);
+        System.out.println("A Area total e "+ ((37*20) + (10*10) + (6*7) ) );
+        System.out.println("FIM DO DEMORADO");
+        //divisão
+        System.out.println("Divisão e "+ ((27/3)*5));
+          System.out.println("FIM DA DIVISÃO" );
+
 
     }
 
