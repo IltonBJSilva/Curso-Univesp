d32ca3567f082db1f0921c810b830f405993dce8
Ilton Batista <43584705+IltonBJSilva@users.noreply.github.com>
2018-10-02 15:04:56 -0300

    Aula 07-Junção de area da casa com da piscina

--- Aula7.java
+++ Aula7.java
@@ -1,8 +1,25 @@
 public class Aula7{
 
   public static void main(String[] args){
+//JUNÇÃO D
+    float lateral = 11;
+    float cquarto = 7
+    float areaq;
+    float areas;
+    float areat;
+    double raio = 2;
+    double areap;
 
-
+    System.out.println("PROGRAMA PARA CALCULO DE AREA DA CASA");
+    areas = lateral*lateral;
+    System.out.println("A area da sala é "+areas);
+    areaq= cquarto*(lateral/2);
+    System.out.println("A area do quarto é "+areaq);
+    System.out.println("A area do banheiro é "+areaq);
+    areat = areas+2*areaq;
+    System.out.println("A area total é "+areat);
+    areap = Math.pow(raio,2);
+    System.out.println("..."+ areap);
 
 
 
